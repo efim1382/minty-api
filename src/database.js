6c7c53b13b275e25144ecf6fa153e01e706e50a8
Roman Yefymov <roman.yefymov@newfold.com>
2023-10-12 21:58:48 +0300

    fix

--- src/database.js
+++ src/database.js
@@ -8,7 +8,7 @@ const pool = new Pool({
 	host: process.env.DB_HOST,
 	database: process.env.DB_NAME,
 	password: process.env.DB_PASSWORD,
-	port: parseInt(process.env.DB_PORT, 10),
+	port: process.env.DB_PORT,
 });
 
 export const query = (text, params) => pool.query(text, params);

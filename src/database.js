9574de36b6376ef9f24076bfe043e4aaf36cfd57
Roman Yefymov <roman.yefymov@newfold.com>
2023-10-13 21:13:28 +0300

    ignore no ssh

--- src/database.js
+++ src/database.js
@@ -16,6 +16,10 @@ const pool = new Pool({
 	database: process.env.DB_NAME,
 	password: process.env.DB_PASSWORD,
 	port: process.env.DB_PORT,
+
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
 export const query = (text, params) => pool.query(text, params);

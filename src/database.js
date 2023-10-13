da30aa64b838036e41b586b863c95f8e1ffa5101
Roman Yefymov <roman.yefymov@newfold.com>
2023-10-13 19:06:08 +0300

    show data

--- src/database.js
+++ src/database.js
@@ -3,6 +3,16 @@ import { Pool } from "pg";
 
 dotenv.config();
 
+const obj = {
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT,
+};
+
+console.log(obj);
+
 const pool = new Pool({
 	user: process.env.DB_USER,
 	host: process.env.DB_HOST,

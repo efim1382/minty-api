537df563fb4d5946c1e40461b6168c5484337552
Roman Yefymov <roman.yefymov@newfold.com>
2023-10-31 18:34:42 +0200

    remove unused endpoint

--- src/controllers/auth/routes.js
+++ src/controllers/auth/routes.js
@@ -3,7 +3,6 @@ import { register, login, getCsrfToken, logout } from "./index";
 
 const router = express.Router();
 
-router.get("/token/", getCsrfToken);
 router.post("/register/", register);
 router.post("/login/", login);
 router.post("/logout/", logout);
